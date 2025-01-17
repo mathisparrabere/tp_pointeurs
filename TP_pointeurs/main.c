5fe07ba89f5a0680bdb174e893787de11fa9644c
Mathis Parrabere <mathis.parrabere@student.junia.com>
2025-01-17 14:17:49 +0100

    commit test pointeurs

--- TP_pointeurs/main.c
+++ TP_pointeurs/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-	const int a = 150;
-	a = 151;
+	int a = 150;
+	printf("%d\n", &a);
+
+	int* p = &a;
+	printf("%d\n", &p);
+	printf("%d\n", p);
+	printf("%d\n", *p);
+
+	*p = 25;
+	printf("%d", a);
 }
