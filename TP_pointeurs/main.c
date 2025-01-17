964c273a55ab091c69cc08d6060be10855dad0ca
Mathis Parrabere <mathis.parrabere@student.junia.com>
2025-01-17 14:47:26 +0100

    Etape 1

--- TP_pointeurs/main.c
+++ TP_pointeurs/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main() {
-	
+	typedef struct {
+		const char* option;
+	} Menu;
+
+	typedef struct {
+		const char* nom;
+		Menu* menus;
+		int nb_menu;
+	} Application;
+
+	Menu* option = "options";
+	Application* nom = "Twitter";
 }
