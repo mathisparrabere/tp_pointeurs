daf3b63565d38ea391f36e2f7326f6428801be22
Mathis Parrabere <mathis.parrabere@student.junia.com>
2025-01-17 14:27:30 +0100

    commit test minmax

--- TP_pointeurs/main.c
+++ TP_pointeurs/main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
+int minmax(int tab[], int taille, int* min, int* max) {
+	*min = tab[0];
+	*max = tab[0];
+
+	for (int i = 0; i < taille; i++) {
+		if (tab[i] < *min) {
+			*min = tab[i];
+		}
+		if (tab[i] > *max) {
+			*max = tab[i];
+		}
+	}
+	return 0;
+}
+
 int main() {
-	int a = 150;
-	printf("%d\n", &a);
-
-	int* p = &a;
-	printf("%d\n", &p);
-	printf("%d\n", p);
-	printf("%d\n", *p);
-
-	*p = 25;
-	printf("%d", a);
+	int min = 0;
+	int max = 0;
+	int tab[] = {-2, 24, 12, -5, 156, 4};
+	minmax(tab, 6, &min, &max);
+	printf("Min : %d et Max : %d", min, max);
+	return 0;
 }
